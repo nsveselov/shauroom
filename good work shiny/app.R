ec0fb3584a826ccae6efd2ee67a90c7202383a58
ruslan-efimov <rvefimov5@gmail.com>
2017-06-08 01:16:49 +0300

    asdfasg

--- good work shiny/app.R
+++ good work shiny/app.R
@@ -1,6 +1,10 @@
 library(leaflet)
 library(shiny)
-library(readr)
+library(RCurl)
+storage = clean_data_v3
+egarots = unique_idshava
+egarots$X <- NA
+
 
 runApp(
   shinyApp(
@@ -19,26 +23,20 @@ runApp(
           # textOutput("Showcase"),
           textOutput("hummary"),
           textOutput("summary"),
+          tags$script(HTML("")),
           tags$script(type="text/javascript", HTML("VK.init({apiId: 6063999});")),
           tags$div(id="vk_auth"),
           tags$script(type="text/javascript",
                       HTML("VK.Widgets.Auth('vk_auth',
-                           {authUrl: 'http://r.piterdata.ninja/p/5060/'});")))
+                           {authUrl: 'http://r.piterdata.ninja/p/7902/'});")))
         # tags$script(type="text/javascript",
         #             HTML("VK.Widgets.Auth('vk_auth', {onAuth: function(data)
         #                  {alert('user '+data['uid']+' authorized');} });")))
         
-      )
+          )
     ),
     
     server = function(input, output, session){
-      
-      #пихаем датку (надо переделать для паблиша?)
-      clean_data_v3 <- read_csv("~/shauroom/clean_data_v3.csv")
-      unique_idshava <- read_csv("~/shauroom/unique_idshava.csv")
-      storage = clean_data_v3
-      egarots = unique_idshava
-      egarots$X <- NULL
       
       # output$text <- renderText({
       #   query <- parseQueryString(session$clientData$url_search)
@@ -61,10 +59,13 @@ runApp(
         actionButton("rateac", "Подтвердить")
         # submitButton("Update View", icon("refresh"))
       )})
+      
       output$summary <- renderText({
+        if (input$rateac == 0)
+          return()
         data = unique_idshava # загрузили данные
         event <- input$map_marker_click # считали нажатие на маркер
-        input$rateac # нажатие на кнопку "Подтвердить" запускает скрипт
+        #input$rateac # нажатие на кнопку "Подтвердить" запускает скрипт
         name = data[data$idshava == event$id, "name"] # получаем название шавки
         address = data[data$idshava == event$id, "right_addres"] # получаем адрес шавки
         isolate(egarots[egarots$idshava == event$id, "X"] <<- input$ratesl) # робит, но с ошибками
@@ -72,7 +73,7 @@ runApp(
                ', адрес: ', address,
                ', название: ', name,
                ', оценка ', isolate(input$ratesl)) # выводим текст
-        }) 
+      }) 
       
       output$hummary <- renderText({
         if ((input$groups == 0) | (length(parseQueryString(session$clientData$url_search)) == 0))
@@ -80,9 +81,9 @@ runApp(
         input$groups # нажатие на кнопку "Подтвердить" запускает скрипт
         isolate(query <- parseQueryString(session$clientData$url_search))
         isolate(getURL(paste('https://api.vk.com/method/users.getSubscriptions?user_id=', query$uid[1], 
-                                                 "&extended=0&fields=id,name", "&v=5.62", sep = ""))) # выводим текст
+                             "&extended=0&fields=id,name", "&v=5.62", sep = ""))) # выводим текст
       }) 
-
+      
       
       
       ### тоже рабочий вариант
@@ -93,7 +94,7 @@ runApp(
       #   actionButton("Go3", "Go3")})
       # observeEvent(input$Go3, {
       #   text <<- paste0(text, "\n", "Button 3 is fully reactive.")})
-
+      
       
       ### хз что это. что это?!
       output$script <- renderUI({
@@ -121,24 +122,24 @@ runApp(
                          
                          observer.observe(target, config);
                          '))})
-
-            
+      
+      #tags$script(HTML('alert(document.URL)'))
       ### первоначальный вариант (п. 1)      
       ### код для вставки попапов
       # popupMaker <- function(id){
       #   a = as.character(uiOutput(id))
       #   b = strsplit(a, " ")
       #   paste(b[[1]][1], b[[1]][2], "style='min-width:10000px;max-height:500px'", b[[1]][3])}
-
+      
       # рисуем карту
       data = unique_idshava
       output$map <- renderLeaflet({
         leaflet(data) %>%
           addProviderTiles(providers$Stamen.TonerLite,
                            options = providerTileOptions(noWrap = TRUE)) %>% 
-      ### первоначальный вариант (п. 2)
-      #     addMarkers(~coord1, ~coord2, layerId = c(data$idshava),
-      #                popup = lapply(paste0("popup", 1:3), popupMaker))})
+          ### первоначальный вариант (п. 2)
+          #     addMarkers(~coord1, ~coord2, layerId = c(data$idshava),
+          #                popup = lapply(paste0("popup", 1:3), popupMaker))})
           addMarkers(~coord1, ~coord2, layerId = c(data$idshava),
                      popup = "<div id=\"popup\" style='min-width:10000px;max-height:500px'
                      class=\"shiny-html-output\"></div>")})
@@ -148,6 +149,6 @@ runApp(
       
       }
         ), launch.browser = TRUE
-)
+        )
 
 # https://rstudio.github.io/leaflet/shiny.html можно сделать карту на весь экран
